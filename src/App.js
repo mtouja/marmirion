3567c1cc4dcc5f4b1d368bf621ed9c21d68d2342
Marion <m.t.touja@gmail.com>
2019-01-05 11:51:54 +0100

    Suppression du composant Login inutile dans la v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Recipes from './Components/Recipes/Recipes';
-// import Login from './Components/Login/Login';
 
 import './App.css';
 
@@ -9,8 +8,7 @@ class App extends Component {
   render() {
     return (
       <div >
-        {/* <Navigation /> */}
-        {/* <Login /> */}
+        <Navigation />
         <Recipes />
       </div>
     );
