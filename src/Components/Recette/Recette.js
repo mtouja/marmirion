4b1f4c72c45361c0e0f142b837203020162d5423
Marion <m.t.touja@gmail.com>
2019-01-18 11:05:01 +0100

    mise à jour en manuel de toute la base de données => base clean pour travailler sur le formaulaire de création de recette.

--- src/Components/Recette/Recette.js
+++ src/Components/Recette/Recette.js
@@ -28,25 +28,49 @@ class Recette extends Component {
             <CardImg top src={this.props.picture} alt='recette picture' className="w-100 mx-auto d-block" />
               <img src={this.props.icon} className="icon mt-3"/>
             <CardBody>
-            <CardTitle className="title-recette mt-3">{this.props.title}</CardTitle>
+            <CardTitle className="title-recette mt-3" style={{minHeight: 38 +'px'}}>{this.props.title}</CardTitle>
               <div className="d-flex justify-content-center align-items-center">
                 <Button outline size="lg" className="mb-3 details" onClick={this.toggle}>{this.props.buttonLabel}Faire cette recette</Button>
                   <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
                     <ModalBody className="description" toggle={this.toggle}>
                       <Card>
+                        <CardTitle className="mt-3 etapes">ingredients</CardTitle>
                         <CardBody>
                           {this.props.ingredients.map((item, index) => {
-                            return <p className="ml-3 mt-3 mb-3">{item.name}{" "}{item.quantité}</p>
+                            return(
+                              <div>
+                                <p className="ml-3 mt-3 mb-3">{item.name}{':'}{' '}{item.quantité}{' '}{item.unity}</p>
+                              </div>
+                            )
                           })}
                         </CardBody>
                       </Card>
                       <Card>
-                        <CardTitle className="mt-3 etapes">étapes de préparation</CardTitle>
                         <CardBody className="p-3">
                           <img src='https://image.flaticon.com/icons/svg/186/186146.svg' className="marmite"/>
                             {this.props.steps.map((details, index) => {
-                              return <p p className="ml-3 mt-3 mb-3">{details.description}</p>
+                              return(
+                                <div>
+                                  <p p className="ml-3 mt-3 mb-3"><strong>{details.name}</strong></p>
+                                  <p p className="ml-3 mt-3 mb-3">{details.description}</p>
+                                </div>
+                              )
                           })}
+                          <div>
+                          {this.props.temps.map((details, index) => {
+                              return(
+                                <div className="text-center">
+                                  {details.thermostat ? 
+                                    <img src="https://image.flaticon.com/icons/svg/808/808444.svg" className="baking"/>
+                                    :
+                                    <img src="https://image.flaticon.com/icons/svg/264/264018.svg" className="baking"/>
+                                  }                                  
+                                  <p p className="ml-3 mt-3 mb-3">{details.duree}{' min'}</p>
+                                  <h6 p className="ml-3 mt-3 mb-3">{details.thermostat ? details.thermostat + ' °C' : null}</h6>
+                                </div>
+                              )
+                          })}
+                          </div>
                           <div className="d-flex justify-content-center align-items-center">
                             <Button color="secondary" className="mt-4 mb-3 details" onClick={this.toggle}>Retour à la liste</Button>
                           </div>
